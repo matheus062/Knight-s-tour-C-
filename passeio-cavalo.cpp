800e5cf4748f2068ecf65995f29aa386eb2a92f4
Luis Carlos <sombra@qualyteam.com.br>
2023-02-04 19:32:24 -0300

    Logica resolvida

--- passeio-cavalo.cpp
+++ passeio-cavalo.cpp
@@ -3,8 +3,9 @@
 using namespace std;
 
 int const SIZE = 8;
-int const xMove[SIZE] = {1, 2, 2, 1, -1, -2, -2, -1};
-int const yMove[SIZE] = {2, 1, -1, -2, -2, -1, 1, 2};
+int const xMove[] = { 2, 1, -1, -2, -2, -1, 1, 2 };
+int const yMove[] = { 1, 2, 2, 1, -1, -2, -2, -1 };
+
 
 struct KnightResult {
 	int moves = 0;
@@ -14,49 +15,47 @@ struct KnightResult {
 void print(int matrix[SIZE][SIZE]) {
 	for (int i = 0; i < SIZE; i++) {
 		for (int j = 0; j < SIZE; j++) {
-			cout << matrix[i][j] << ' ';
+			cout << matrix[i][j] << "  ";
 		}
 
 		cout << endl;
 	}
-
 }
 
 bool trial_error(int board[SIZE][SIZE], int x, int y, int pos) {
 	//KnightResult result;
 
-	if (pos == 64) {
-		return true;
-	}
-	
-	system("cls");
-	print(board);
-
-	bool run;
-
-	int xNext = 0;
-	int yNext = 0;
-
-
-	if ((x >= 0) && (x < SIZE) && (y >= 0) && (y < SIZE) && (board[x][y] == 0)) {
-
-		board[x][y] = pos;
-		pos++;
-
-		for (int i = 0; i < SIZE; i++) {
-			xNext = x + xMove[i];
-			yNext = y + yMove[i];
-
-			// Caso seja falso, tente a nova posição, caso chegue a todas e não consigue
-			// Faça o backtracking
-			return trial_error(board, xNext, yNext, pos);
+	try {
+		if (pos == 64) {
+			return true;
 		}
-	}
-	else {
-		return false;
-	}
 
-	return true;
+		
+		for (int i = 0; i < SIZE; i++) {
+			int xNext = x + xMove[i];
+			int yNext = y + yMove[i];
+
+			if ((x >= 0) && (x < SIZE) && (y >= 0) && (y < SIZE) && (board[xNext][yNext] == 0)) {
+				board[xNext][yNext] = pos;
+
+
+				if (trial_error(board, xNext, yNext, pos + 1))
+					return true;
+
+				// Caso seja falso, tente a nova posição, caso chegue a todas e não consigue
+				// Faça o backtracking
+				board[xNext][yNext] = 0;
+
+
+
+			}
+		}
+		return false;
+
+	}
+	catch (exception e) 
+	{
+	}
 }
 
 
@@ -67,8 +66,10 @@ int main()
 	KnightResult result1;
 	int board[SIZE][SIZE];
 	fill(*board, *board + 64, 0);
-
 	trial_error(board, 0, 0, 1);
+
+	system("cls");
+	print(board);
 
 	return 0;
 }
